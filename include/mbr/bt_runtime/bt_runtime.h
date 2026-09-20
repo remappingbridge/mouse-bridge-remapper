46753bae9d49587ca0db006ac56b2326a41d18b2
tiagooliveirajs <tiagooliveirajs@yahoo.com>
2026-09-19 23:04:56 -0300

    MBR-05: add BLE runtime ownership and queue

---
+++ include/mbr/bt_runtime/bt_runtime.h
@@ -0,0 +1,36 @@
+#ifndef MBR_BT_RUNTIME_BT_RUNTIME_H
+#define MBR_BT_RUNTIME_BT_RUNTIME_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MBR_BT_RUNTIME_MESSAGE_PAYLOAD_SIZE 64u
+#define MBR_BT_RUNTIME_QUEUE_CAPACITY 128u
+
+typedef struct {
+    uint16_t channel;
+    uint16_t type;
+    uint16_t length;
+    uint8_t payload[MBR_BT_RUNTIME_MESSAGE_PAYLOAD_SIZE];
+} mbr_bt_runtime_message_t;
+
+typedef void (*mbr_bt_runtime_session_setup_fn)(void);
+
+void mbr_bt_runtime_reset(void);
+bool mbr_bt_runtime_publish(uint16_t channel,
+                            uint16_t type,
+                            const void *payload,
+                            uint16_t length);
+bool mbr_bt_runtime_poll(mbr_bt_runtime_message_t *message);
+bool mbr_bt_runtime_take_overflow(void);
+bool mbr_bt_runtime_start(mbr_bt_runtime_session_setup_fn session_setup);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
